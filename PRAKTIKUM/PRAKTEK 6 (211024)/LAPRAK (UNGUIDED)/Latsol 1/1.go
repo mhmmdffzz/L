492af18f2216253924833d5b13af7b5d4d2c8f3e
Muhammad Fauzan <111763204+mhmmdffzz@users.noreply.github.com>
2024-11-26 10:58:54 +0700

    1.go

--- PRAKTIKUM/PRAKTEK 6 (211024)/LAPRAK (UNGUIDED)/Latsol 1/1.go
+++ PRAKTIKUM/PRAKTEK 6 (211024)/LAPRAK (UNGUIDED)/Latsol 1/1.go
@@ -5,18 +5,21 @@ import (
 )
 
 func main() {
-	var parsel, berat_asli, berat, biaya, sisa int
+	var berat_asli int
 	fmt.Print("Berat parsel (gram): ")
 	fmt.Scanln(&berat_asli)
-	parsel = berat_asli / 1000
-	berat = berat_asli % 1000
-	fmt.Printf("Detail berat: %d kg + %d gr\n", parsel, berat)
-	biaya = parsel * 10000
-	if berat < 500 {
-		sisa = berat * 15
-	} else if berat >= 500 {
-		sisa = berat * 5
+	kg := berat_asli / 1000
+	gram := berat_asli % 1000
+	fmt.Printf("Detail berat: %d kg + %d gr\n", kg, gram)
+	biaya_kg := kg * 10000
+	
+	var biaya_gram int
+	if gram >= 500 {
+		biaya_gram = gram * 5
+	} else {
+		biaya_gram = gram * 15
 	}
-	fmt.Printf("Detail biaya: Rp. %d + Rp. %d \n", biaya, sisa)
-	fmt.Printf("Total biaya: Rp. %d", biaya+sisa)
+
+	fmt.Printf("Detail biaya: Rp. %d + Rp. %d\n", biaya_kg, biaya_gram)
+	fmt.Printf("Total biaya: Rp. %d", biaya_kg+biaya_gram)
 }
